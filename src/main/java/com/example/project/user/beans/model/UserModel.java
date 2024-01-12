12d4751919de0570c37a2ec75cd02154347eee2f
jaehyeon2 <wol5971@gmail.com>
2024-01-12 16:33:32 +0900

    commit

---
+++ src/main/java/com/example/project/user/beans/model/UserModel.java
@@ -0,0 +1,13 @@
+package com.example.project.user.beans.model;
+
+import java.io.Serializable;
+
+public class UserModel implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	private String UserId;
+	
+	private String UserNo;
+	
+}
---
+++ src/main/java/com/example/project/user/beans/param/ParamS.java
@@ -0,0 +1,7 @@
+package com.example.project.user.beans.param;
+
+
+public class ParamS{
+
+	
+}
