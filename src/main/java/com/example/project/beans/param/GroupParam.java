14b050427f171dae7e664e4d3302af781635be08
jaehyeon2 <wol5971@gmail.com>
2024-01-14 17:37:55 +0900

    additional setting

---
+++ src/main/java/com/example/project/beans/param/GroupParam.java
@@ -0,0 +1,28 @@
+package com.example.project.beans.param;
+
+public class GroupParam {
+	
+	private String groupId;
+	private String groupNo;
+	private String groupName;
+	
+	public String getGroupId() {
+		return groupId;
+	}
+	public void setGroupId(String groupId) {
+		this.groupId = groupId;
+	}
+	public String getGroupNo() {
+		return groupNo;
+	}
+	public void setGroupNo(String groupNo) {
+		this.groupNo = groupNo;
+	}
+	public String getGroupName() {
+		return groupName;
+	}
+	public void setGroupName(String groupName) {
+		this.groupName = groupName;
+	}
+
+}
--- src/main/java/com/example/project/beans/param/UserParam.java
+++ src/main/java/com/example/project/beans/param/UserParam.java
@@ -1,7 +1,56 @@
 package com.example.project.beans.param;
 
-
-public class UserParam{
-
+public class UserParam extends GroupParam{
+	
+	private String userId;
+	private String userNo;
+	private String userName;
+	private String userRank;
+	private String userPosition;
+	private String userPwd;
+	private String userPhone;
+	
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserNo() {
+		return userNo;
+	}
+	public void setUserNo(String userNo) {
+		this.userNo = userNo;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserRank() {
+		return userRank;
+	}
+	public void setUserRank(String userRank) {
+		this.userRank = userRank;
+	}
+	public String getUserPosition() {
+		return userPosition;
+	}
+	public void setUserPosition(String userPosition) {
+		this.userPosition = userPosition;
+	}
+	public String getUserPwd() {
+		return userPwd;
+	}
+	public void setUserPwd(String userPwd) {
+		this.userPwd = userPwd;
+	}
+	public String getUserPhone() {
+		return userPhone;
+	}
+	public void setUserPhone(String userPhone) {
+		this.userPhone = userPhone;
+	}
 	
 }
